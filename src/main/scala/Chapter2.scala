2420fd2aee8f619172feb3788a02ca488d6232d5
Andrew Jones <ximenean@hotmail.com>
2014-10-05 19:17:04 +0100

    Initial commit

---
+++ src/main/scala/Chapter2.scala
@@ -0,0 +1,66 @@
+object MyModule {
+
+  def abs(n : Int) : Int =
+    if (n < 0) -n
+    else n
+
+  def factorial(n : Int) : Int = {
+    @annotation.tailrec
+    def go(n : Int, acc : Int) : Int =
+      if (n <= 0) acc
+      else  go(n - 1, n * acc)
+
+    go(n, 1)
+
+  }
+
+  def fib(n : Int) : Int = {
+    @annotation.tailrec
+    def go(n : Int, a : Int, b : Int) : Int =
+      go(n - 1, b, a + b)
+
+    go(n, 0, 1)
+  }
+
+  def findFirst[A](ss : Array[A], p : A => Boolean) : Int = {
+    @annotation.tailrec
+    def loop(n : Int) : Int =
+      if (n >= ss.length) - 1
+      else if (p(ss(n))) n
+      else loop(n + 1)
+
+    loop(0)
+
+  }
+
+  def isSorted[A](as : Array[A], ordered : (A, A) => Boolean) : Boolean = {
+    @annotation.tailrec
+    def loop(n : Int) : Boolean =
+      if ( n + 1 >= as.length) true
+      else if ( !ordered(as(n), as(n+1)) ) false
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def partial1[A, B, C](a : A, f: (A,B) => C): B => C =
+    (b : B) => f(a, b)
+
+  def curry[A, B, C](f : (A, B) => C): A => (B => C) =
+    (a : A) => (b : B) => f(a,b)
+
+  def uncurry[A, B, C](f : A => B => C): (A, B) => C =
+    (a : A, b : B) => f(a)(b)
+
+  def compose[A, B, C](f : B => C, g : A => B): A => C =
+    (a : A) => f(g(a))
+
+  def formatResult(name : String, n : Int, f : Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]) : Unit =
+    println(formatResult("absolute value", -42, abs))
+
+}
