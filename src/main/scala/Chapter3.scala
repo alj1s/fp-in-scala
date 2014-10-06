b4ae39046ee16124eaed52368b59890c86e2eeb8
Andrew Jones <ximenean@hotmail.com>
2014-10-06 21:02:50 +0100

    Work through up to (and including) Exercise 3.6

---
+++ src/main/scala/Chapter3.scala
@@ -0,0 +1,72 @@
+/**
+ * Created by andrewjones on 06/10/2014.
+ */
+package fpinscala.datastructures
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, xs) => xs
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] = {
+    def loop(l: List[A], n: Int): List[A] =
+      if (n == 0) l
+      else loop(tail(l), n - 1)
+
+    loop(l, n)
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(x, xs) if (f(x)) => dropWhile(xs, f)
+    case _ => l
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => a2
+    case Cons(h, t) => Cons(h, append(t, a2))
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  def setHead[A](l: List[A], a: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, xs) => Cons(a, xs)
+  }
+
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case _ => 101
+
+  }
+}
+
+
