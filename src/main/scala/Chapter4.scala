d7b3d9955d1afdc073d000dc02f6f12eaac2521b
Andrew Jones <ximenean@hotmail.com>
2014-12-21 20:37:59 +0000

    Completed Chapter 4

---
+++ src/main/scala/Chapter4.scala
@@ -0,0 +1,131 @@
+/**
+ * Created by andrewjones on 21/12/14.
+ */
+package fpinscala.datastructures.Chapter4
+
+// Exercise 4.1
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(x) => f(x)
+    case None => None
+  }
+
+  def flatMap2[B](f: A => Option[B]): Option[B] = {
+    map(f) getOrElse None
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def orElse2[B >: A](ob: => Option[B]): Option[B] = {
+    this map (Some(_)) getOrElse ob
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(x) if (f(x)) => this
+    case _ => None
+  }
+
+  def filter2(f: A => Boolean): Option[A] = {
+    flatMap(x => if (f(x)) Some(x) else None)
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+
+object None extends Option[Nothing]
+
+object Option {
+
+  // Exercise 4.2
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
+  // Exercise 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (None, _) => None
+    case (_, None) => None
+    case (Some(x), Some(y)) => Some(f(x, y))
+  }
+
+  def map2_flatMap[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  }
+
+  // Exercise 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => None
+    case x :: xs => x flatMap (xx => sequence(xs) map (xx :: _))
+  }
+
+  // Exercise 4.5
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => None
+    case x :: xs => map2_flatMap(f(x), traverse(xs)(f))(_ :: _)
+  }
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)(x => x)
+  }
+}
+
+// Exercise 4.6
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(x) => Left(x)
+    case Right(x) => Right(f(x))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(x) => Left(x)
+    case Right(x) => f(x)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(x) => b
+    case Right(x) => this
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+  }
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+
+  // Exercise 4.7
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
+    traverse(es)(x => x)
+  }
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case Nil => Right(Nil)
+    case x :: xs => (f(x) map2 traverse(xs)(f))(_ :: _)
+  }
+}
+
