036839ec0d33aa6075fbd82e2ffe8ffa0349f883
Andrew Jones <ximenean@hotmail.com>
2014-12-26 17:45:38 +0000

    Complete Chapter 6

--- src/main/scala/Chapter6.scala
+++ src/main/scala/Chapter6.scala
@@ -116,5 +116,91 @@ object RNG {
   }
 
   def ints2(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
+
+  // Exercise 6.8
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, r) = f(rng)
+      g(a)(r)
+    }
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    rng =>
+      val (i, r) = nonNegativeInt(rng)
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) (mod, r)
+      else nonNegativeLessThan(n)(rng)
+  }
+
+  // Exercise 6.9
+  def map_1[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit[B](f(a)))
+
+  def map2_1[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
+
+  def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_ + 1)
+
 }
 
+
+import State._
+
+// Exercise 6.10
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => sb.map(b2 => f(a, b2)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
+object State {
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+  //type Rand[A] = State[RNG, A]
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+}
+
+sealed trait Input
+
+case object Coin extends Input
+
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Candy {
+
+  // Exercise 6.11
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs.map(i => modify((m: Machine) => (i, m) match {
+      case (_, Machine(_, 0, _)) => m
+      case (Coin, Machine(false, _, _)) => m
+      case (Turn, Machine(true, _, _)) => m
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })))
+    m <- get
+  } yield (m.coins, m.candies)
+}
